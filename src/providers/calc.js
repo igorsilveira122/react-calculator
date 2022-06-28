eee04d5eeb550e9feddfd544fd089f565e97c6c9
Monique <moques3@outlook.com>
2022-06-28 10:54:56 -0300

    funcao de calculo

--- src/providers/calc.js
+++ src/providers/calc.js
@@ -1,25 +1,54 @@
 import { createContext, useState } from "react";
+import Button from "../Button/Button";
+import Display from "../Display/Display";
 
 export const CalcContext = createContext([]);
 
 export function CalcProvider({ children }) {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState('0');
 
     function changeValue(newValue) {
-        setValue(newValue);
+        if (value.includes('/') || value.includes('+') || value.includes('-') || value.includes('*')) {
+            setValue(value + newValue);
+        }
+        else {
+            if (value != '0' && value.length >= 1) {
+                setValue(value + newValue);
+            }
+
+            else 
+                setValue(newValue + '');
+        }
     }
 
     function clean() {
-        setValue(0);
+        setValue('0');
+    }
+
+
+    function calc() {
+        let parts = value.split('+')
+        setValue (parseInt(parts[0]) + parseInt(parts[1]) + '');
     }
 
     function operation(operator) {
-        alert(operator);
+        if (operator == 'AC') {
+            clean();
+        }
+        else if (operator == '=') {
+            calc()
+        }
+
+        else {
+            setValue(value + operator);
+        }
     }
+
+
 
     return (
         <CalcContext.Provider value={{ value, changeValue, operation }}>
             {children}
         </CalcContext.Provider>
     );
-}
+    }
